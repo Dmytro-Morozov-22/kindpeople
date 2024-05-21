8ea54de9df7f5cad98ed1f8fc4504983df702db2
Dmytro-Morozov-22 <morozovdima6@gmail.com>
2024-05-21 23:50:25 +0300

    Created basic entities User, Post, Comment, ImageModel, and enum ERole. Created repositories UserRepository, PostRepository, ImageRepository, CommentRepository.

---
+++ server-kindpeople/src/main/java/com/dmtrmrzv/kindpeople/KindPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.dmtrmrzv.kindpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindPeopleApplication.class, args);
+	}
+
+}
---
+++ server-kindpeople/src/main/java/com/dmtrmrzv/kindpeople/entity/User.java
@@ -0,0 +1,62 @@
+package com.dmtrmrzv.kindpeople.entity;
+
+import com.dmtrmrzv.kindpeople.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Data
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, updatable = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(columnDefinition = "text")
+    private String bio;
+
+    @Column(length = 3000)
+    private String password;
+
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",  joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name")
+    private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    private List<Post> post = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public  User() {}
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+
+
+}
